4d68e88ec88777e9ecf2d8f734d436047610a68e
Mikhail Chekov <chekoff@list.ru>
2021-03-04 08:16:15 +0300

    feat: implemented min, max and avg methods

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!arguments.length)return 0;
+  if(!array.length) return 0;
+
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!arguments.length)return 0;
+  if(!array.length) return 0;
+
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!arguments.length)return 0;
+  if(!array.length) return 0;
+
+  let sum = array.reduce((ac, e, i) => {
+    return ac += e;
+  });
+
+  return sum/array.length;
 }
